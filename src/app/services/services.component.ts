a20ec374f7df012744935652018fca9e1fb0e90c
Georgi Maksimov <georgi.binchev@infinno.eu>
2023-07-01 00:23:05 +0300

    add services

--- src/app/services/services.component.ts
+++ src/app/services/services.component.ts
@@ -1,13 +1,52 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { Component } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { MatCardModule } from "@angular/material/card";
+import { animate, style, transition, trigger } from "@angular/animations";
 
 @Component({
-  selector: 'app-services',
+  selector: "app-services",
   standalone: true,
-  imports: [CommonModule],
-  templateUrl: './services.component.html',
-  styleUrls: ['./services.component.scss']
+  imports: [CommonModule, MatCardModule],
+  templateUrl: "./services.component.html",
+  styleUrls: ["./services.component.scss"],
+  animations: [
+    trigger('slideInOut', [
+      transition(':enter', [
+        style({ transform: 'translateX(-500%)' }),
+        animate('1000ms ease', style({ transform: 'translateX(0)' }))
+      ]),
+      transition(':leave', [
+        animate('1000ms ease', style({ transform: 'translateX(-500%)' }))
+      ])
+    ])
+  ]
 })
 export class ServicesComponent {
-
+  services: any[] = [
+    {
+      title: "СЕРВИЗНО ОБСЛУЖВАНЕ",
+    },
+    {
+      title: "СПИРАЧНА СИСТЕМА",
+    },
+    {
+      title: "КЛИМАТИК",
+    },
+    {
+      title: " ИЗПУСКАТЕЛНА СИСТЕМА",
+    },
+    {
+      title: "АНГРЕНАЖЕН РЕМЪК",
+    },
+    {
+      title: " ОКАЧВАНЕ",
+    },
+    {
+      title: "МЕХАНИЧНИ РЕМОНТИ",
+    },
+    {
+      title: " ПРОВЕРКА НА МАСЛОТО",
+    },
+    
+  ];
 }
