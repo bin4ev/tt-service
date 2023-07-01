4c606e79db33f3e68991e07718d9c1fc6f50a82f
Georgi Maksimov <georgi.binchev@infinno.eu>
2023-07-02 01:19:23 +0300

    add contacts

--- src/app/contacts/contacts.component.ts
+++ src/app/contacts/contacts.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+
 
 @Component({
   selector: 'app-contacts',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, MatFormFieldModule, MatInputModule,
+    MatIconModule, MatButtonModule],
   templateUrl: './contacts.component.html',
   styleUrls: ['./contacts.component.scss']
 })
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,7 +1,7 @@
-import { Component } from "@angular/core";
+import { Component, ElementRef, ViewChild } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { MatButtonModule } from "@angular/material/button";
-import { animate, style, transition, trigger } from "@angular/animations"
+import { animate, style, transition, trigger } from "@angular/animations";
 
 @Component({
   selector: "app-home",
@@ -10,21 +10,30 @@ import { animate, style, transition, trigger } from "@angular/animations"
   templateUrl: "./home.component.html",
   styleUrls: ["./home.component.scss"],
   animations: [
-    trigger('slideInOut', [
-      transition(':enter', [
-        style({ transform: 'translateX(-100%)' }),
-        animate('1000ms ease', style({ transform: 'translateX(0)' }))
+    trigger("slideInOut", [
+      transition(":enter", [
+        style({ transform: "translateX(-100%)" }),
+        animate("1000ms ease", style({ transform: "translateX(0)" })),
       ]),
-      transition(':leave', [
-        animate('1000ms ease', style({ transform: 'translateX(-100%)' }))
-      ])
-    ])
-  ]
+      transition(":leave", [
+        animate("1000ms ease", style({ transform: "translateX(-100%)" })),
+      ]),
+    ]),
+  ],
 })
 export class HomeComponent {
+  @ViewChild("background") background!: ElementRef;
   isShow = false;
 
   showMoreInfo() {
     this.isShow = true;
+    this.unblurBackground();
+  }
+
+  unblurBackground() {
+    let filter = this.background.nativeElement.style.filter;
+    if (filter != "none") {
+      this.background.nativeElement.style.filter = 'none'
+    }
   }
 }
