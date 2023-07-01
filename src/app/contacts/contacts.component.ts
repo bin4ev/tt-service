0c9693b13418a09256206d48583905bbe6c9ca6c
Georgi Maksimov <georgi.binchev@infinno.eu>
2023-07-02 01:21:51 +0300

    add animation

--- src/app/contacts/contacts.component.ts
+++ src/app/contacts/contacts.component.ts
@@ -4,6 +4,7 @@ import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatInputModule} from '@angular/material/input';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
+import { animate, style, transition, trigger } from '@angular/animations';
 
 
 @Component({
@@ -12,7 +13,18 @@ import { MatButtonModule } from '@angular/material/button';
   imports: [CommonModule, MatFormFieldModule, MatInputModule,
     MatIconModule, MatButtonModule],
   templateUrl: './contacts.component.html',
-  styleUrls: ['./contacts.component.scss']
+  styleUrls: ['./contacts.component.scss'],
+  animations: [
+    trigger('slideInOut', [
+      transition(':enter', [
+        style({ transform: 'translateX(-100%)' }),
+        animate('1000ms ease', style({ transform: 'translateX(0)' }))
+      ]),
+      transition(':leave', [
+        animate('1000ms ease', style({ transform: 'translateX(-100%)' }))
+      ])
+    ])
+  ]
 })
 export class ContactsComponent {
 
