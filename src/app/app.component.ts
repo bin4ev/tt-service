677795023eaa07a906ca261cbe070647083348b2
Georgi <georgi.binchev@sirmabc.com>
2024-05-19 00:07:41 +0300

    fix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,7 @@ import { RouterOutlet } from '@angular/router';
 import { TranslateService } from '@ngx-translate/core';
 import { HeaderComponent } from './shared/header/header.component';
 import { FooterComponent } from './shared/footer/footer.component';
+import { BookAppointmentBtnComponent } from './shared/components/book-appointment-btn/book-appointment-btn.component';
 
 
 @Component({
@@ -10,7 +11,7 @@ import { FooterComponent } from './shared/footer/footer.component';
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.scss'],
     standalone: true,
-    imports: [HeaderComponent, RouterOutlet, FooterComponent,]
+    imports: [HeaderComponent, RouterOutlet, FooterComponent,BookAppointmentBtnComponent]
 })
 export class AppComponent {
   #translateService = inject(TranslateService)
